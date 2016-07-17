89dd5632336a7d5216c5b566e1d70285c713cff9
honzi <czechinthemail@gmail.com>
2016-07-17 16:29:26 -0700

    switched to common random.js usage

--- js/duel.js
+++ js/duel.js
@@ -21,7 +21,7 @@ function duel(){
       && healths[1] > 0
       && turn_counter < 100){
         for(var player in players){
-            if(random_number(100) < players[player]['health-regen%']
+            if(random_integer(100) < players[player]['health-regen%']
               && healths[player] < players[player]['health']){
                 healths[player] = Math.min(
                   players[player]['health'],
@@ -44,7 +44,7 @@ function duel(){
                 continue;
             }
 
-            if(random_number(100) < players[player]['mana-regen%']
+            if(random_integer(100) < players[player]['mana-regen%']
               && manas[player] < players[player]['mana']){
                 manas[player] = Math.min(
                   players[player]['mana'],
@@ -59,8 +59,8 @@ function duel(){
                   + players[player]['mana'];
             }
 
-            if(random_number(100) <= players[player]['hit%']
-              && random_number(100) >= players[1 - player]['dodge%']){
+            if(random_integer(100) <= players[player]['hit%']
+              && random_integer(100) >= players[1 - player]['dodge%']){
                 var damage = Math.max(0, players[player]['damage'] - players[1 - player]['defense']);
 
                 healths[1 - player] -= damage;
@@ -124,10 +124,6 @@ function load(){
     players = JSON.parse(imported);
 
     setmode(0);
-}
-
-function random_number(i){
-    return Math.floor(Math.random() * i);
 }
 
 function reset(skip){
