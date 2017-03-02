3eb6cb696fcc92e6e306f150f579a43e2d2ce971
honzi <czechinthemail@gmail.com>
2017-03-02 14:23:37 -0800

    fixed random_integer_max usage

--- js/duel.js
+++ js/duel.js
@@ -21,9 +21,7 @@ function duel(){
       && healths[1] > 0
       && turn_counter < 100){
         for(var player in players){
-            if(random_integer({
-              'max': 100,
-            }) < players[player]['health-regen%']
+            if(random_integer() < players[player]['health-regen%']
               && healths[player] < players[player]['health']){
                 healths[player] = Math.min(
                   players[player]['health'],
@@ -46,9 +44,7 @@ function duel(){
                 continue;
             }
 
-            if(random_integer({
-              'max': 100,
-            }) < players[player]['mana-regen%']
+            if(random_integer() < players[player]['mana-regen%']
               && manas[player] < players[player]['mana']){
                 manas[player] = Math.min(
                   players[player]['mana'],
@@ -63,12 +59,8 @@ function duel(){
                   + players[player]['mana'];
             }
 
-            if(random_integer({
-              'max': 100,
-            }) <= players[player]['hit%']
-              && random_integer({
-                'max': 100,
-              }) >= players[1 - player]['dodge%']){
+            if(random_integer() <= players[player]['hit%']
+              && random_integer() >= players[1 - player]['dodge%']){
                 var damage = Math.max(0, players[player]['damage'] - players[1 - player]['defense']);
 
                 healths[1 - player] -= damage;
